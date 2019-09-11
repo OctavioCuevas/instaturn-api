702d41cd397fa1e44f02503ead6e05070170e9b3
tavuu <octaviocuevas@outlook.com>
2019-09-10 21:32:10 -0600

    Corregir PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
     const express = require('express');
     const app = express();
     const bodyParser = require('body-parser');
-    const PORT = process.env._PORT || 5000;
+    const PORT = process.env.PORT || 5000;
     require('./database/index.js');
     const User = require('./models/UserSchema');
     const Ticket = require('./models/TicketSchema');
